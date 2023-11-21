405303c716ace8c958e012ebb21364dfdf5ba1af
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-11-21 16:06:05 -0500

    initial commit  with models and detail and some views completed

--- django_marketplace/django_marketplace/urls.py
+++ django_marketplace/django_marketplace/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core.views import index 
+from django.urls import path, include
+from core.views import index
+from core.views import contact
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('admin/', admin.site.urls),
-]
+    path("", include('core.urls')),
+    path("items/", include("item.urls")),
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
