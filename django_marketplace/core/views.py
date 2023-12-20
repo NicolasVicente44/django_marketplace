cf9e9443b1bc58b5f8ced7853cb5a0a003ecf0b9
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-12-20 10:40:11 -0500

    added contact and about page

--- django_marketplace/core/views.py
+++ django_marketplace/core/views.py
@@ -19,6 +19,8 @@ def index(request):
 
 def contact(request):
     return render(request, "core/contact.html")
+def about(request):
+    return render(request, "core/about.html")
 
 
 def signup(request):
